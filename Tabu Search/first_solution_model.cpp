594c8f01116a5ac4f7990b7bebd467c99599b8a6
valdomiro <valdomiroapc@github.com>
2021-08-16 01:04:11 -0300

    adding first solution model files

---
+++ Tabu Search/first_solution_model.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <set>
+#include <map>
+#include <string>
+#include <stack>
+#include "instance_data.h"
+#include "first_solution_model.h"
+using namespace std;
+#define ull unsigned long long int
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define fast_io cin.tie(0), cin.sync_with_stdio(false);
+#define PI 3.14159265359
+#define eps 1e-9
+#define unvisited -1
+typedef vector<int>vi;
+typedef pair<int,int> ii;
+typedef pair<int,ii> iii;
+typedef vector<ii> vii;
+typedef vector<iii> viii;
+typedef pair<int, string> istr;
+typedef vector<istr> vistr;
+typedef vector< vector<int> > vvi;
+typedef vector< vector<ii> > vvii;
